c13a9e983e2a9e3985f962d5be5f27b6a48ed19e
bsima <bensima@gmail.com>
2014-01-05 13:17:32 -0500

    Started building out scraper logic and view

--- app.rb
+++ app.rb
@@ -2,9 +2,12 @@ require 'mechanize'
 require 'sinatra/base'
 require 'haml'
 
-
+# A class is needed to make
+# Mechanize play nice with Sinatra.
 class Aristotl < Sinatra::Base
     set :haml, :format => :html5
+
+    # @TODO handle 404's and other errors
 
     get '/' do
         haml :index # @TODO make an index page
@@ -15,16 +18,38 @@ class Aristotl < Sinatra::Base
     end
 
     get '/entry/:entry' do
-        # Scrape Plato for `:slug`
-        # Grab the article content and display it
-        agent    = Mechanize.new
-        url      = "http://plato.stanford.edu/entries/" + params[:entry] + "/"
-        page     = agent.get(url)
+        # Load the page
+        agent = Mechanize.new
+        url   = "http://plato.stanford.edu/entries/" + params[:entry] + "/"
+        page  = agent.get(url)
 
-        # @TODO I need to edit the `$content` using regex's on `img src=` to
+        # @TODO Build error handling for when the requested entry doesn't exist on the SEP
+
+        # Let's do some page scraping.
+        #
+        # @TODO I need to edit the `$content` (using regex's?) on `img src=` to
         #       properly hotlink images back to the plato.stanford.edu website.
         #       Or, just remove the image if it's unnecessary.
+        $entry_title = page.search("h1")
+        $entry_pub   = page.search("div#pubinfo")
+
+        # @TODO This is gonna be a bit harder. I need every `p` element before the
+        #       first `hr` element.
+        $entry_intro = page.search("")
+
+        $entry_toc = page.search("ul:first-of-type")
+        
+        # @TODO This ain't gonna be easier either. I now need all of the content
+        #       after the `hr` element.
         $content = page.search("div#aueditable")
+        
+        # Generate the page!
         haml :entry
+    end
+
+    get '/api/:entry' do
+        # Planned feature: An API for accessing the SEP info in JSON, XML, etc format
+        #
+        # I have no idea how I'm gonna do this, but whatevs.
     end
 end
