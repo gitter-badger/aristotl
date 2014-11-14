bc828e42f2c21ab3d2ef41f7a717a261e2cb98d9
bsima <bensima@gmail.com>
2014-11-14 10:57:12 -0500

    Delete old Ruby files. Starting from scratch

--- public/images/favicon/generate_favicon.sh
+++
@@ -1,7 +0,0 @@
-convert favicon.png -bordercolor white -border 0 \
-    \( -clone 0 -resize 16x16 \) \
-    \( -clone 0 -resize 32x32 \) \
-    \( -clone 0 -resize 48x48 \) \
-    \( -clone 0 -resize 64x64 \) \
-    \( -clone 0 -resize 128x128 \) \
-    -delete 0 -alpha off -colors 256 favicon.ico
