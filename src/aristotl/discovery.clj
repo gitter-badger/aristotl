1692a4c9de34a11605a7b49a337fa92811255387
Ben Sima <bensima@gmail.com>
2015-05-18 16:02:10 -0400

    refocus

--- src/aristotl/discovery.clj
+++ src/aristotl/discovery.clj
@@ -2,8 +2,10 @@
  "The discovery service."
  (:require [net.cgrand.enlive-html :as html]))
 
-; start with a data structure
-(defrecord Article [title pubdate lede toc content])
+
+(defrecord SEPArticle [title pubdate lede toc content]
+  ; function signatures:
+  (article [args] "Docstring"))
 
 (def   base-url         "http://plato.stanford.edu/")
 (def   index-url        (str base-url "contents.html"))
@@ -13,7 +15,8 @@
 (def index-links
   (let [a-tags   (html/select index [:div#content :ul :li :a])
         entries  (map #(get-in % [:attrs :href]) a-tags)
-        sharp?   #(re-find #"^#.+" %)
+        sharp?   #(re-find #"^#.+" %) ;remove links to subsections
         filtered (remove sharp? entries)
         links    (map make-url filtered)]
     links))
+
