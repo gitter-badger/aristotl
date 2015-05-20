e3cf2689cc96405f04e7381578036ee08285464e
Ben Sima <bensima@gmail.com>
2015-05-19 20:04:11 -0400

    Finish converting my random functions into structured protocols

--- src/aristotl/discovery.clj
+++ src/aristotl/discovery.clj
@@ -3,20 +3,34 @@
  (:require [net.cgrand.enlive-html :as html]))
 
 
-(defrecord SEPArticle [title pubdate lede toc content]
-  ; function signatures:
-  (article [args] "Docstring"))
+(defprotocol DataSource
+  (base-url    [this] "The base-url") 
+  (index-url   [this] "If available, a complete listing of all the contents of the data source.")
+  (index-links [this] "If availalbe, a parsed list of all the links in the index."))
 
-(def   base-url         "http://plato.stanford.edu/")
-(def   index-url        (str base-url "contents.html"))
-(defn- make-url  [slug] (str base-url slug))
-(defn  fetch     [url]  (html/html-resource (java.net.URL. url)))
 
-(def index-links
-  (let [a-tags   (html/select index [:div#content :ul :li :a])
+(defprotocol Article
+  (url     [this] "Returns the url of the Article")
+  (fetch   [this] "Gets the articles and returns the complete HTTP response (for parsing with enlive)")
+  (content [this] "Returns the content of the article"))
+
+
+(defrecord SEP [slug]
+
+  DataSource
+  (base-url    [this] "http://plato.stanford.edu/")  
+  (index-url   [this] (str base-url "contents.html"))
+  (index-links [this]
+    (let [a-tags   (html/select index [:div#content :ul :li :a])
         entries  (map #(get-in % [:attrs :href]) a-tags)
         sharp?   #(re-find #"^#.+" %) ;remove links to subsections
         filtered (remove sharp? entries)
         links    (map make-url filtered)]
     links))
+  
+  Article
+  (url     [this] (str base-url "entry/" (:slug this)))
+  (fetch   [this] (html/html-resource (java.net.URL. (url (:slug this)))))
+  (content [this] (fetch (:slug this))))
+
 
--- src/aristotl/parser.clj
+++
@@ -1,6 +0,0 @@
-(ns aristotl.parser
-  (:require [net.cgrand.enlive-html :refer :all]
-            [aristotl.spider        :refer :all]))
-
-;; right now the parser just works with SEP
-
